ba2ad813cd73884dbc34a30cacf73583dd4c5bf3
YvenChang <chenyifan.gg@gmail.com>
2017-03-23 00:02:20 +0800

    change data storage

--- README.md
+++ README.md
@@ -8,11 +8,8 @@
 
 zaima是一个无聊的练手，用来在命令行中查看[摸鱼王Quin](https://www.douyu.com/3614)有没有在播。
 
-对zaima执行``$ chmod a+x zaima.py``之后，将文件路径加入到系统环境变量中。
-
-喜欢的话可以去掉后缀py，直接输入``$ zaima``执行。
+直接输入``$ zaima.py``执行。
 
 ## Request
 
 1. [requests](https://github.com/kennethreitz/requests)
-2. [PyYAML](http://pyyaml.org/wiki/PyYAML)
--- zaima.py
+++ zaima.py
@@ -1,7 +1,6 @@
 #! /usr/local/bin/python3
 # coding=utf-8
 import requests
-import time
 import random
 import datetime
 
@@ -9,42 +8,56 @@ import datetime
 url = 'http://open.douyucdn.cn/api/RoomApi/room/'
 roomNum = '3614'
 
-# Quin luanguage
-quinLanguage = [
-    "摸了。",
-    "秦先森已经歇了。",
-    "什么？已经八点了？",
-    "靠妖啊，秦先森又在吃香香鸡了。",
-    "秦喵喵再不播我就要死了。",
-    "zaima",
-    "不在，cnm",
-    "戳木娘啊，秦川又去哪里摸鱼了。",
-    "你能表演一下那个吗？",
-    "每天都在那里直播。",
-    "狗才摸鱼。",
-    "别问了，秦川这种生物不存在的。",
-    "zaima？男娼堇业代表。",
-    "怕不是要摸爆一切。",
-    "怕不是要摸到时间的尽头。",
-    "秦川什么时候来舞动青春啊。",
-    "说不出话。"
-]
 
-# Quin name
-quinName = [
-    "秦川",
-    "秦先森",
-    "秦喵喵",
-    "秦智障",
-    "川川子",
-    "鸡盒摸鱼王奎恩",
-    "Quin",
-    "缺",
-    "缺哥哥",
-    "缺神",
-    "Q酱",
-    "二五仔"
-]
+# 计算播的时间数据，存入 dict 中
+def bo_time(roomData):
+    boData = {}
+    boData['nonB'] = datetime.datetime.now() - \
+        datetime.datetime.strptime(
+            roomData.get('start_time'), '%Y-%m-%d %H:%M')
+    boData['days'] = boData['nonB'].days
+    boData['hours'] = boData['nonB'].seconds / (3600)
+    boData['min'] = (boData['hours'] - int(boData['hours'])) * 60
+    boData['sec'] = (boData['min'] - int(boData['min'])) * 60
+    return boData
+
+
+# Quin luanguage
+# quinLanguage = [
+#     "摸了。",
+#     "秦先森已经歇了。",
+#     "什么？已经八点了？",
+#     "靠妖啊，秦先森又在吃香香鸡了。",
+#     "秦喵喵再不播我就要死了。",
+#     "zaima",
+#     "不在，cnm",
+#     "戳木娘啊，秦川又去哪里摸鱼了。",
+#     "你能表演一下那个吗？",
+#     "每天都在那里直播。",
+#     "狗才摸鱼。",
+#     "别问了，秦川这种生物不存在的。",
+#     "zaima？男娼堇业代表。",
+#     "怕不是要摸爆一切。",
+#     "怕不是要摸到时间的尽头。",
+#     "秦川什么时候来舞动青春啊。",
+#     "说不出话。"
+# ]
+#
+# # Quin name
+# quinName = [
+#     "秦川",
+#     "秦先森",
+#     "秦喵喵",
+#     "秦智障",
+#     "川川子",
+#     "鸡盒摸鱼王奎恩",
+#     "Quin",
+#     "缺",
+#     "缺哥哥",
+#     "缺神",
+#     "Q酱",
+#     "二五仔"
+# ]
 
 # get API return
 # deal with the return data
@@ -57,21 +70,15 @@ except requests.RequestException as e:
 
 roomError = r.get('error')
 
-# now time
-ntime = time.strftime('%Y%m%d', time.localtime(time.time()))
-ntimeS = time.strftime('%H', time.localtime(time.time()))
-
 # roomError == 0 then catch the right data
 if roomError == 0:
     # catch the room data in returned JSON data
+    quinLanguage = open("./langs/quinLangs.dat", "r").readlines()
+    quinName = open("./langs/quinName.dat", "r").readlines()
     roomData = r.get('data')
     quinName = random.choice(quinName)
-    nonBS = 0
 
     # get the last play time
-    btime = datetime.datetime.strptime(
-        roomData.get('start_time'), '%Y-%m-%d %H:%M')
-    # print(type(bitime))
 
     print("%s说：%s" % (quinName, roomData.get('room_name')))
     print()
@@ -79,17 +86,14 @@ if roomError == 0:
     # not in play
     if roomData.get('room_status') == '2':
         # if 1:
-        nonB = (datetime.datetime.now() - btime)
-        bDays = nonB.days
-        bHours = nonB.seconds / (3600)
-        bMin = (bHours - int(bHours)) * 60
-        bSec = (bMin - int(bMin)) * 60
+        bo = bo_time(roomData)
+        # print(bo)
 
-        if not bDays:
+        if not bo['days']:
             print("刚刚勃完，让%s歇一歇吧，不要猝死在直播间。" % quinName)
         else:
-            print(quinName, "已经摸了", bDays, " 天", int(bHours),
-                  "个小时", int(bMin), "分钟", int(bSec), "秒了。")
+            print(quinName, "已经摸了", bo['days'], " 天", int(bo['hours']),
+                  "个小时", int(bo['min']), "分钟", int(bo['sec']), "秒了。")
             print(random.choice(quinLanguage))
     elif roomData.get('room_status') == '1':
         print("惊了！")
