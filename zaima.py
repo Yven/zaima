3cf0944e295df79424ef399a9bb19c979fb98b4a
Joker Qyou <Joker.Qyou@gmail.com>
2017-04-10 21:22:57 +0800

    Fix  variable names

--- zaima.py
+++ zaima.py
@@ -55,11 +55,11 @@ class zaima:
             else:
                 print("%s已经摸了%s天%s个小时%s分钟%s秒了。" % (self.__quinName, self.__boData[
                       'days'], self.__boData['hours'], self.__boData['min'], self.__boData['sec']))
-                print(quinLanguage)
+                print(self.__quinLanguage)
         elif self.__roomData['room_status'] == '1':
             print("惊了！")
-            print(quinName, "居然播了，不敢信。")
-            print("而且有", roomData.get('online'), "个猛男在看直播，整个房间都gay gay的。")
+            print(self.__quinName, "居然播了，不敢信。")
+            print("而且有", self.__roomData.get('online'), "个猛男在看直播，整个房间都gay gay的。")
 
 
 if __name__ == '__main__':
