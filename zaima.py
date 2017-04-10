01cd9a6cd688e2b23a6cb6622cd3b52f5c7b6742
YvenChang <chenyifan.gg@gmail.com>
2017-04-10 10:49:39 +0800

    first

--- zaima.py
+++ zaima.py
@@ -2,7 +2,8 @@
 # coding=utf-8
 import requests
 import random
-import datetime
+from datetime import datetime
+from os import path
 
 
 class zaima:
@@ -28,9 +29,9 @@ class zaima:
     # 计算播的时间数据，存入 dict 中
     def __bo_time(self):
         self.__boData = {}
-        self.__boData['nonB'] = datetime.datetime.now() - \
-            datetime.datetime.strptime(
-                self.__roomData.get('start_time'), '%Y-%m-%d %H:%M')
+        self.__boData['nonB'] = datetime.now() - \
+            datetime.strptime(self.__roomData.get(
+                'start_time'), '%Y-%m-%d %H:%M')
         self.__boData['days'] = self.__boData['nonB'].days
         self.__boData['hours'] = self.__boData['nonB'].seconds / (3600)
         self.__boData['min'] = (self.__boData['hours'] -
@@ -42,10 +43,16 @@ class zaima:
     def __read_langs(self):
         self.langsFile = "./langs/quinLangs.dat"
         self.nameFile = "./langs/quinName.dat"
-        self.__quinLanguage = random.choice(
-            open(self.langsFile, "r").readlines()).strip()
-        self.__quinName = random.choice(
-            open(self.nameFile, "r").readlines()).strip()
+        if path.exists(self.langsFile) and path.exists(self.nameFile):
+            self.__quinLanguage = random.choice(
+                open(self.langsFile, "r").readlines()).strip()
+            self.__quinName = random.choice(
+                open(self.nameFile, "r").readlines()).strip()
+        else:
+            self.__quinLanguage = random.choice(
+                open("/Users/yven/Dropbox/Workspace/Github/zaima/langs/quinLangs.dat", "r").readlines()).strip()
+            self.__quinName = random.choice(
+                open("/Users/yven/Dropbox/Workspace/Github/zaima/langs/quinName.dat", "r").readlines()).strip()
 
     def message_output(self):
         self.__read_langs()
@@ -59,15 +66,16 @@ class zaima:
             else:
                 print("%s已经摸了%s天%s个小时%s分钟%s秒了。" % (self.__quinName, self.__boData[
                       'days'], self.__boData['hours'], self.__boData['min'], self.__boData['sec']))
-                print(quinLanguage)
+                print(self.__quinLanguage)
         elif self.__roomData['room_status'] == '1':
             print("惊了！")
-            print(quinName, "居然播了，不敢信。")
-            print("而且有", roomData.get('online'), "个猛男在看直播，整个房间都gay gay的。")
+            print(self.__quinName, "居然播了，不敢信。")
+            print("而且有", self.__roomData.get('online'), "个猛男在看直播，整个房间都gay gay的。")
 
 
 if __name__ == '__main__':
     # douyu API adderss
+    # TODO: 日历系统，为秦先生每天签到吧！
     zaima = zaima()
     if not zaima.error:
         zaima.message_output()
